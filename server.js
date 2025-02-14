4186707c2e59dc1621df16d7715c4f25946ca0ab
unknown <CU@B1-315-E15299.cuchdit.in>
2025-02-14 13:36:51 +0530

    cors issue

--- server.js
+++ server.js
@@ -1,21 +1,37 @@
 const express = require("express");
 const app = express();
 const connectDB = require("./db");
-require('dotenv').config();
+require("dotenv").config();
+const cors = require("cors");
 
 const bodyParser = require("body-parser");
 app.use(bodyParser.json());
 
+//middle ware
+
+const logRequest = (req, res, next) => {
+  console.log(
+    `[${new Date().toLocaleString()}] Request received for ${req.originalUrl}`
+  );
+  next();
+};
+app.use(logRequest);
+
+const allowOrigin = [
+  "http://localhost:3000",
+  "https://harmonious-llama-0bcd0f.netlify.app/",
+];
+
+app.use(cors({ origin: allowOrigin, credentials: true }));
+
 const menuItemRouter = require("./routes/menuItemRouter");
-app.use("/menu-item", menuItemRouter);
-
 const personRoute = require("./routes/personRoute");
-app.use("/person", personRoute);
-
 const bookingRouter = require("./routes/BookingRouter");
-app.use("/booking", bookingRouter);
-
 const userRouter = require("./routes/userRoute");
+
+app.use("/menu-item", menuItemRouter);
+app.use("/person", personRoute);
+app.use("/booking", bookingRouter);
 app.use("/user", userRouter);
 
 connectDB();
@@ -24,8 +40,7 @@ app.get("/", function (req, res) {
   res.send("hello world");
 });
 
-
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log("Server Listining on :", PORT);
 });
